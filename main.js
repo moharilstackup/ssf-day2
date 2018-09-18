5863c6bd9a51fcd1c157625f927ce46571522ed8
Mohd Shahril <moharilstackup@gmail.com>
2018-09-18 14:20:13 +0800

    Done with day2

--- main.js
+++ main.js
@@ -6,20 +6,89 @@ const path = require("path");
 const app = express();
 
 //Step 3: define routes
+// Log every request that comes in
+app.use((req, resp, next) => {
+    console.log(`****** ${new Date()}, ${req.method}, ${req.originalUrl}`);
+    next();
+});
+
+//app.use(express.static('public'))
+//app.use(express.static('files'))
+// GET /image/cat.png  -> HTTP
+//app.use(express.static('/image'));
+// app.use(express.static(__dirname + "/image"));
+// app.get("/image", (req, resp) => {
+//     resp.status(202);
+//     resp.type('jpg');
+//     resp.sendfile("cat.jpg");
+// });
+app.get('/image/cat.jpg', (req, resp) => {
+    //status
+    resp.status(203);
+    resp.type('jpg');
+    resp.sendfile("cat.jpg");
+    // console.log(`Response type, ${resp.status}`);
+ });
+
+ app.get('/image/:animal', function(req, resp) {
+    const animal = req.params.animal;
+    console.log(`Animal ${animal}`);
+    resp.status(208);
+    resp.type('jpg');
+    resp.sendfile(req.params.animal);
+/*     findUserByUsername(username, function(error, user) {
+      if (error) return next(error);
+      return response.render('user', user);
+    }); */
+  });
+
+ 
 // GET /time -> HTTP
-app.get('/time',(req, resp) => {
+app.get('/time',
+    (req, resp, next) => {
+        console.log(new Date() + ' Processing time')
+        next();
+    },
+    (req, resp) => {
+        //status
+        resp.status(201);
+        resp.type('text/html');
+        resp.send(`<h1>The current time is ${new Date()}</h1>`);
+    }
+);
+
+// GET /time/json
+app.get('time/json', (req, resp) => {
+    const data = { 
+        "name" : "fred",
+        "email": "fred@gmm.com"
+    }
     //status
     resp.status(200);
-    resp.type('text/html');
-    resp.send(`<h1>The current time is ${new Date()}</h1>`);
+    resp.type('application/json'); //media type
+    resp.json({
+        time: new Date()
+       // people : data
+    });
+    // resp.send(`<h1>The current time is ${new Date()}</h1>`);
 });
+
 // GET / -> HTTP
-app.get('/',(req, resp) => {
+app.get('/', (req, resp) => {
     const userAgent = req.get('User-Agent');
     //status
     resp.status(200);
     resp.type('text/html');
     resp.send(`<h1>Hello ${userAgent}</h1>`);
+});
+
+// GET .* -> HTTP
+app.get('*', (req, resp) => {
+    const userAgent = req.get('User-Agent');
+    //status
+    resp.status(208);
+    resp.type('text/html');
+    resp.send(`<h1>Error MAN</h1>`);
 });
 
 //Serves from public
